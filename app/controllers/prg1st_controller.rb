33e18c8f5322d3ad15e5fe4de76b107d7d8ac048
keito uriu <kei2000123@icloud.com>
2021-06-21 16:08:47 +0000

    aaaa

---
+++ app/controllers/prg1st_controller.rb
@@ -0,0 +1,4 @@
+class Prg1stController < ApplicationController
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'hello' => 'home#index'
+  get 'Prg-1st' => 'prg1st#index'
 end
